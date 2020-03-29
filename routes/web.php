4827b6c9a9af03e6d6333fa7d29b4ca82c73e427
huypro <vuquochuy04@gmail.com>
2020-03-29 09:20:05 +0700

    admin master page setup

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,11 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
+Route::get('/admin',function(){
 
+    return view('admin.index');
+
+});
 
 
 Route::resource('admin/users','AdminUsersController');
